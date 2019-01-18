f5957b2dbdc6b9d88d758a4ebcf702e96dcacee8
Hanaasagi <ambiguous404@gmail.com>
2019-01-18 23:59:43 +0900

    call curl in subprocess

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[macro_use]
 extern crate log;
 extern crate chrono;
+extern crate tempfile;
+
+use tempfile::NamedTempFile;
 use std::env;
 use std::process::exit;
+use std::process::Command;
+use std::collections::HashMap;
 
 mod logging;
 use logging::{init_logger};
@@ -17,6 +22,8 @@ use logging::{init_logger};
         //}
     //}
 //}
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 static HELP: &'static str = r#"
 Usage: httpstat URL [CURL_OPTIONS]
@@ -43,6 +50,23 @@ Environments:
                         from current shell $PATH.
   HTTPSTAT_DEBUG        Set to `true` to see debugging logs. Default is `false`
 "#;
+
+
+static curl_format: &'static str = r#"
+    "time_namelookup": %{time_namelookup},
+    "time_connect": %{time_connect},
+    "time_appconnect": %{time_appconnect},
+    "time_pretransfer": %{time_pretransfer},
+    "time_redirect": %{time_redirect},
+    "time_starttransfer": %{time_starttransfer},
+    "time_total": %{time_total},
+    "speed_download": %{speed_download},
+    "speed_upload": %{speed_upload},
+    "remote_ip": "%{remote_ip}",
+    "remote_port": "%{remote_port}",
+    "local_ip": "%{local_ip}",
+    "local_port": "%{local_port}"
+}"#;
 
 
 fn print_help() {
@@ -86,4 +110,102 @@ fn main() {
         );
 
     debug!("httpstat debug mode enabled");
+
+    // env debug
+    debug!(
+        "flag:{{\
+        show_body={show_body}, \
+        show_ip={show_ip}, \
+        show_speed={show_speed}, \
+        save_body={save_body}, \
+        curl_bin={curl_bin}}}",
+        show_body=show_speed,
+        show_ip=show_ip,
+        show_speed=show_speed,
+        save_body=save_body,
+        curl_bin=curl_bin,
+    );
+
+    let first_arg = &args[0];
+    if first_arg == "-h" || first_arg == "--help" {
+        print_help();
+        exit(0);
+    } else if first_arg == "--version" {
+        println!("httpstat {version}", version=VERSION);
+        exit(0);
+    }
+
+    let url: &str = &first_arg;
+    let curl_args: Vec<&str> = args[1..].iter().map(|s| s as &str).collect();
+
+    let exclude_options = [
+        "-w", "--write-out",
+        "-D", "--dump-header",
+        "-o", "--output",
+        "-s", "--silent"
+    ];
+
+    for option in exclude_options.iter() {
+        if curl_args.contains(&option) {
+            println!("Error: {option} is not allowed in extra curl args", option=option);
+            exit(1);
+        }
+    }
+
+
+    let headerf = NamedTempFile::new()
+        .unwrap_or_else(
+            |e| {
+                println!("create tempfile failed: {}", e);
+                exit(1)
+            }
+        );
+
+    let bodyf = NamedTempFile::new()
+        .unwrap_or_else(
+            |e| {
+                println!("create tempfile failed: {}", e);
+                exit(1)
+            }
+        );
+
+    let mut cmd_env = env::vars().collect::<HashMap<String, String>>();
+    cmd_env.entry("LC_ALL".into()).or_insert("C".into());
+
+    let mut cmd_core = vec![
+        //curl_bin,
+        "-w",
+        curl_format,
+        "-D",
+        headerf.path().to_str().unwrap(),  // TODO
+        "-o",
+        bodyf.path().to_str().unwrap(),
+        "-s",
+        "-S"
+    ];
+
+    let mut cmd = vec![];
+    cmd.extend(cmd_core);
+    cmd.extend(curl_args);
+    cmd.extend(vec![url]);
+
+    debug!("cmd: {:?}", cmd);
+
+    // invoke curl command
+
+    let output =
+        Command::new(curl_bin)
+        .args(&cmd)
+        .envs(&cmd_env)
+        .output()
+        .expect("failed to execute process");
+    debug!("process exited with: {}", output.status);
+
+    let stdout = String::from_utf8(output.stdout)
+        .expect("invalid UTF-8");
+    let stderr = String::from_utf8(output.stderr)
+        .expect("invalid UTF-8");
+
+    debug!("stdout: {}", stdout);
+    debug!("stderr: {}", stderr);
 }
